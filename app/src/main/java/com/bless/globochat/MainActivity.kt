b64a529dc0f7de15fbdbb3f70f8aedb08b2ddda6
Bless Pearl <mbohblesspearl@gmail.com>
2021-11-27 12:37:28 +0100

    creating settings screen with code

--- app/src/main/java/com/bless/globochat/MainActivity.kt
+++ app/src/main/java/com/bless/globochat/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.bless.globochat
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -11,7 +13,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.preference.PreferenceManager
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , SharedPreferences.OnSharedPreferenceChangeListener{
 
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -32,9 +34,40 @@ class MainActivity : AppCompatActivity() {
         val sharedPreferences= PreferenceManager.getDefaultSharedPreferences(this)
         val autoReplyTime= sharedPreferences.getString(getString(R.string.key_auto_reply_time),"")
         Log.i("MainActivity", "Auto Reply Time: $autoReplyTime")
+
+        val publicInfo: Set<String>?=sharedPreferences.getStringSet(getString(R.string.key_public_info),null)
+        Log.i("MainActivity", "Public Info: $publicInfo ")
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onSharedPreferenceChanged(p0: SharedPreferences?, p1: String?) {
+     if(p1 == getString(R.string.key_status)){
+         val newStatus = p0?.getString(p1,"")
+         Toast.makeText(this, "New Status $newStatus", Toast.LENGTH_SHORT).show()
+     }
+
+        if(p1==getString(R.string.key_auto_reply)){
+            val autoReply=p0?.getBoolean(p1, false)
+            if(autoReply!!){
+                Toast.makeText(this, "AutoReply:ON", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this, "AutoReply:OFF", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        PreferenceManager.getDefaultSharedPreferences(this).unregisterOnSharedPreferenceChangeListener(this)
+
     }
 }
